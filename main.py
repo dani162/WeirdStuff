6671fe9e245957ac3f4c04aaa216078479c13f88
dani162 <{ID}+{username}@users.noreply.github.com>
2022-05-17 23:11:57 +0200

    added random_game_loop

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ clock = pygame.time.Clock()
 debug_draw_path = True
 debug_draw_background = True
 
+random_percentage = 0.10
+
 # init stuff
 pygame.init()
 pygame.font.init()
@@ -35,7 +37,7 @@ is_first_game_cycle = True
 path = []
 
 # possible game modes: 'MANUAL', 'SQUARE_SPIRAL', 'SPIRAL' and 'RANDOM'
-game_mode = 'SQUARE_SPIRAL'
+game_mode = 'RANDOM'
 
 
 ####
@@ -79,6 +81,25 @@ def fix_robot_pos(robot):
         robot[1] = 0
     if robot[1] > height - size:
         robot[1] = height - size
+
+
+def get_randomized_velocity(velocity_1=None, velocity_2=None):
+    if not velocity_1:
+        velocity_1 = random.uniform(-1, 1) * speed
+        velocity_2 = math.sqrt(speed**2 - velocity_1**2) * (2 * random.randint(0, 1) - 1)
+        return velocity_1, velocity_2
+    velocity_2_direction = 1
+    if velocity_2 < 0:
+        velocity_2_direction = -1
+    velocity_1 += random.uniform(-1, 1) * speed * random_percentage
+    if velocity_1 > speed:
+        velocity_1 = velocity_1 - (velocity_1 - speed)
+        velocity_2_direction *= -1
+    elif velocity_1 < - speed:
+        velocity_1 = velocity_1 - (velocity_1 + speed)
+        velocity_2_direction *= -1
+    velocity_2 = math.sqrt(speed ** 2 - velocity_1 ** 2) * velocity_2_direction
+    return velocity_1, velocity_2
 
 
 def init_game_cycle():
@@ -172,12 +193,30 @@ def circle_game_loop():
         temp_speed = speed * ms_last_frame / 1000
         angle_speed = temp_speed / math.sqrt(size**2/(4*math.pi**2)+(current_radius**2))
         radius_speed = math.sqrt(temp_speed**2 - current_radius**2 * angle_speed**2)
-        # angle_speed = speed / 200 * ms_last_frame / 1000
-        # radius_speed = size / cycle_time
         current_radius += radius_speed
         current_angle += angle_speed
         robot[0] = start_position[0] + math.cos(current_angle) * current_radius
         robot[1] = start_position[1] + math.sin(current_angle) * current_radius
+        fix_robot_pos(robot)
+        draw(robot)
+
+
+def random_game_loop():
+    print('Random Mode')
+    robot = [(width-size)/2, (height-size)/2]
+    velocity_x, velocity_y = get_randomized_velocity()
+    while game_is_running:
+        ms_last_frame, events = init_game_cycle()
+        robot[0] += velocity_x * ms_last_frame / 1000
+        robot[1] += velocity_y * ms_last_frame / 1000
+        if robot[0] < 0:
+            velocity_x, velocity_y = get_randomized_velocity(velocity_x*-1, velocity_y)
+        if robot[0] > width - size:
+            velocity_x, velocity_y = get_randomized_velocity(velocity_x*-1, velocity_y)
+        if robot[1] < 0:
+            velocity_y, velocity_x = get_randomized_velocity(velocity_y*-1, velocity_x)
+        if robot[1] > height - size:
+            velocity_y, velocity_x = get_randomized_velocity(velocity_y*-1, velocity_x)
         fix_robot_pos(robot)
         draw(robot)
 
@@ -189,6 +228,8 @@ def start():
         square_game_loop()
     elif game_mode == 'SPIRAL':
         circle_game_loop()
+    elif game_mode == 'RANDOM':
+        random_game_loop()
     pygame.quit()
 
 
